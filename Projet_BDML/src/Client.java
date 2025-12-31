889907c5b6c8442060e9a96d035b02ec7374d758
Yo <rabahiyoann@gmail.com>
2025-12-31 14:58:20 +0000

    getters + setters client

--- Projet_BDML/src/Client.java
+++ Projet_BDML/src/Client.java
@@ -13,28 +13,74 @@ public class Client {
     
     private int numero_client;
     private String nom_client;
-    private String num_tel_client;
+    private String tel_client;
     private String mail_client;
     
     // constructeur sans mail
     
-    public Client(int numero_client, String nom_client, String num_tel_client)
+    public Client(int numero_client, String nom_client, String tel_client)
     {
         this.numero_client = numero_client;
         this.nom_client = nom_client;
-        this.num_tel_client = num_tel_client;
+        this.tel_client = tel_client;
         this.mail_client = null;
     }
     
     // constructeur avec mail
     
-    public Client(int numero_client, String nom_client, String num_tel_client, String mail_client)
+    public Client(int numero_client, String nom_client, String tel_client, String mail_client)
     {
         this.numero_client = numero_client;
         this.nom_client = nom_client;
-        this.num_tel_client = num_tel_client;
+        this.tel_client = tel_client;
         this.mail_client = mail_client;
     }
+    
+    // Getters
+    
+    public int getNumeroClient()
+    {
+        return numero_client;
+    }
+    
+    public String getNomClient()
+    {
+        return nom_client;
+    }
+    
+    public String getTelClient()
+    {
+        return tel_client;
+    }
+    
+    public String getMailClient()
+    {
+        return mail_client;
+    }
+    
+    // Setters
+    
+    public void setNumeroClient(int numero_client)
+    {
+        this.numero_client = numero_client;
+    }
+    
+    public void setNomClient(String nom_client)
+    {
+        this.nom_client = nom_client;
+    }
+    
+    public void setTelClient(String tel_client)
+    {
+        this.tel_client = tel_client;
+    }
+    
+    public void setMailClient(String mail_client)
+    {
+        this.mail_client = mail_client;
+    }
+    
+    
     
     
 }

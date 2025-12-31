abca8853e10915bda7881fe5412edcc53c848e33
Yo <rabahiyoann@gmail.com>
2025-12-31 13:14:55 +0000

    attribut + constructeur

--- Projet_BDML/src/Client.java
+++ Projet_BDML/src/Client.java
@@ -7,6 +7,34 @@
  *
  * @author rabah
  */
+
+// un numéro de client, son nom, son numéro de téléphone et, éventuellement, son adresse électronique.
 public class Client {
+    
+    private int numero_client;
+    private String nom_client;
+    private String num_tel_client;
+    private String mail_client;
+    
+    // constructeur sans mail
+    
+    public Client(int numero_client, String nom_client, String num_tel_client)
+    {
+        this.numero_client = numero_client;
+        this.nom_client = nom_client;
+        this.num_tel_client = num_tel_client;
+        this.mail_client = null;
+    }
+    
+    // constructeur avec mail
+    
+    public Client(int numero_client, String nom_client, String num_tel_client, String mail_client)
+    {
+        this.numero_client = numero_client;
+        this.nom_client = nom_client;
+        this.num_tel_client = num_tel_client;
+        this.mail_client = mail_client;
+    }
+    
     
 }

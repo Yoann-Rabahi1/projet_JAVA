44d8c29afe1d65b9ded0bff6160bf4f0f813a41e
Yo <rabahiyoann@gmail.com>
2026-01-01 16:30:04 +0000

    ajout de la classe PrestationSale

---
+++ Projet_BDML/src/PrestationSale.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rabah
+ */
+public class PrestationSale extends Prestation{
+    
+    public PrestationSale(CategorieVehicule categorieVehicule)
+    {
+        super(categorieVehicule);
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "["
+                + " categorie véhicule : " + getCategorieVehicule()
+                + " ]";
+    }
+    
+}
